83863ba516d26fd2ca9e63a5a66be9c390778ab7
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-17 20:53:06 +0000

    printing out the last digit of a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *
+ *Return:0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d and is greater than 5\n", n);
+	}
+	else if ((n % 10) == 0)
+	{
+		printf("Last digit of %d and is 0\n", n);
+	}
+	else if (6 > (n % 10)  > 0)
+	{
+		printf("Last digit of %d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,5 @@
-my first readme file
+| SCRIPT | FUNCTION |
+| ------ | -------- |
+| 0-positive_or_negative.c | prints out the state of the random number,positive,negative otr zero |
+| 1-last_digit,c | prints out the last digit of the random numnber |
+
