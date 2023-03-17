541e8a9452af9291db14ab88af6871f1f616707a
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-17 23:32:18 +0000

    lower and upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return:0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	char letter2 = 'A';
+
+	while (letter <= 'z', letter2 <= 'Z')
+	{
+		putchar(letter);
+		putchar(letter2);
+		letter2++;
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
