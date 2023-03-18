3cf15c08bb4747450dbb950b4c6074e5707f4e47
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-18 00:01:52 +0000

    all letters but q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return:0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,3 +4,4 @@
 | 1-last_digit,c | prints out the last digit of the random numnber |
 | 2 print the alphabet | the program prints the alphabet |
 | 3-print_alphabet | prints lower and upper alphabets |
+| 4-print_alphabet.c | print all alphabet letters but q and e |
