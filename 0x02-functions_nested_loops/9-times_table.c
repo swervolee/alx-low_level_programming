887ba6306821bbd36a987eae21058b8b68b093fe
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-21 22:36:04 +0000

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,8 @@ void times_table(void)
 			c = (a * b);
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
+			_putchar('\n');
 			_putchar(',');
 			_putchar(' ');
 		}
-	_putchar('\n');
 }
