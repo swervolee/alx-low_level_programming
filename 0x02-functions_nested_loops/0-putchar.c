140ce3b2628314e3692eccb055ed0cf70b531375
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-21 10:45:30 +0000

    printing putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <main.h>
-#include <_putchar.c>
+#include "main.h"
+
 /**
- *main - entry point
- *Return:0
+ *main - print _putchar
+ *Return: 0
  */
+
 int main(void)
 {
-	char *x = "_putchar";
-
-	for (int i = 0; x[i] != '\0'; i++)
-	{
-		putchar(x[i]);
-	}
-	putchar('\n');
+	_puchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,5 @@
-s
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c)
+
+#endif
