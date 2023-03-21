a4c772fdf606d0ba9ba4cb36c73a362bbdd86f0c
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-21 12:36:55 +0000

    printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the alphabet in lowercase
+ *Return: 0
+ */
+void print_alphabet(void);
+{
+	for (char x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,4 +3,5 @@
 
 | script | functiom |
 | ------ | --------- |
-| 
+| task 0 | _putchar prototype |
+| task 1 | printing the alphabet |
