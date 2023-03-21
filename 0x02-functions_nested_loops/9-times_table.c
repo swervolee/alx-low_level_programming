5f54d94053591e1beba5c22787c20fe413ea35da
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-21 22:30:51 +0000

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,13 @@ void times_table(void)
 	int c;
 
 	for (a = 0; a <= 9; a++)
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			c = (a * b);
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
+			_putchar(',');
 			_putchar(' ');
 		}
+	_putchar('\n')
 }
