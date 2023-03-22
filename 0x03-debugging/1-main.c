211afbee7ab36979dd996ab0bb049a816889b3ea
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-22 14:39:33 +0000

    commenting a bug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	int i;**/
+	int i;
 
 	printf("Infinite loop incoming :(\n");
 
@@ -16,8 +16,8 @@ int main(void)
 /*	while (i < 10)
  *	{
  *		putchar(i);
- */		}
-
+ *	}
+ */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
