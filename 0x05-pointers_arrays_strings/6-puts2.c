4821d1af15a61a57ea74247eca2044441a8b70b9
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-27 18:01:38 +0000

    printing every other number correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,10 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if ('a' < str[i] < 'z' && 'Z' < str[i] < 'Z')
+		{
+			_putchar(str[i]);
+		}
 		i += 2;
 	}
 	_putchar(10);
