1501b9e4e4b9f4bf10fffe8a63f0936b85e00469
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-28 18:14:22 +0000

    printing strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *_atoi - the filter function
+ *@s: the character
+ *Return: 0
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+	int digit;
+
+	if (s[i] = '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] = +)
+	{
+		sign = +1;
+		i++;
+	}
+	for (; s[i] != '\0' ; i++)
+	{
+		if (isdigit(s[i]))
+		{
+			digit = s[i] - '0';
+			result = result * 10 + digit;
+		}
+	}
+	return (sign * result);
+}
