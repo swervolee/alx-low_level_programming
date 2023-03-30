446c9263e78e15cd570584e414962f58b6fa724d
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-03-30 17:39:42 +0000

    adjustment

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,19 +2,19 @@
 /**
  *rot13 - cipher2
  *@s: character input
- *Return: nul
+ *Return: 0
  */
 char *rot13(char *s)
 {
 	int cnt = 0, i = 0;
-	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char cipher[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (*(s + cnt) != '\0')
 	{
 		for (i = 0; i < 52; i++)
 		{
-			if (*(s + cnt) == alpha[i])
+			if (*(s + cnt) == alph[i])
 			{
 				*(s + cnt) = cipher[i];
 			}
