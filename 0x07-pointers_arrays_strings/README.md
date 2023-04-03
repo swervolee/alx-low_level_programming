9906eae713ad1baa5b44bfd05bf056b6c8da609d
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-03 20:49:04 +0000

    character searching

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -8,4 +8,5 @@
 | task 2 | strspn |
 | task 3 | character location |
 | task 4 | character search |
+| task 5 | character search |
 | 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
