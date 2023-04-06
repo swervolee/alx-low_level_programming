68cc514119ee1326792521c06d892f19b0d34cc9
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-06 21:27:29 +0000

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,12 +9,14 @@ int compare(char *str1, char *str2)
 {
 	if (*str1 == '\0' && *str2 == '\0')
 		return (1);
-	if (*str1 == '\0' && *str2 == '*')
+	else if (*str1 == '\0' && *str2 == '*')
 		return (compare(str1, str2 + 1));
 	else if (*str1 == '\0' && *str2 != '\0')
 		return (0);
 	if (*str1 == *str2)
 		return (compare(str1 + 1, str2 + 1));
+	else
+		return (1);
 }
 
 /**
