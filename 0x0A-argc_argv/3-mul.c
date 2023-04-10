8828939f450b3fdda45dbb94b5159c97e468e353
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-10 20:39:09 +0000

    multipying two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *main - a function to multiply two numbers
@@ -9,40 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-	char e[] = "Error";
-	int sign, i = 0, j = 0, no1 = 0, no2 = 0, sign1, result = 0;
-
-	if (argc < 3)
+	if (argc != 3)
 	{
-		printf("%s\n", e);
+		printf("Error\n");
 		return (1);
 	}
 
-	if (argv[1][i] == '-')
-	{
-		sign = -1;
-		i = 1;
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
-	else if (argv[2][j] == '-')
-	{
-		sign1 = -1;
-		j = 1;
-	}
-
-	while (argv[1][i] != '\0')
-	{
-		no1 = no1 * 10 + (argv[1][i] - '0');
-		i++;
-	}
-	while (argv[2][j] != 0)
-	{
-		no2 = no2 * 10 + (argv[2][j] - '0');
-		j++;
-	}
-
-	result = no1 * no2 * sign * sign1;
-
-	printf("%d", result);
 	return (0);
 }
