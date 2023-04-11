bc1941441d10a67f773ac36fbb93b5b75edfc48b
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-11 20:40:10 +0000

    concanating two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *str_concat - concanates two strings
+ *@s1: the first string
+ *@s2: the second string
+ *Return: a pointer to the concanated strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, n = 0, m = 0;
+	char *cnt;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s2)
+		return (s1);
+	if (!s1)
+		return (s2);
+
+
+	while (*(s1 + i) != '\0')
+		i++;
+
+	while (*(s2 + j))
+		j++;
+
+	cnt = malloc((j + i + 1) * sizeof(char));
+
+	if (cnt != NULL)
+	{
+		while  (n < i)
+		{
+			*(cnt + n) = *(s1 + n);
+			n++;
+		}
+
+		while (m < j)
+		{
+			*(cnt + n + m) = *(s2 + m);
+			m++;
+		}
+
+	}
+
+	*(cnt + n + m) = '\0';
+
+	return (cnt);
+}
