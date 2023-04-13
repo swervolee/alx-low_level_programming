3f2db258edde45ffedfa550e19730f15a0da92aa
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-13 18:49:16 +0000

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	str = malloc(nmemb * size);
-	if (!nmemb)
+	if (!str)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		str[i] = 0;
