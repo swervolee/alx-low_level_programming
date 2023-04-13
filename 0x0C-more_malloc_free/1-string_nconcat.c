3faa9c2be600dd424fdf7e42cadc3d6f8a0ecd73
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-13 16:33:41 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the maximum number of bytes to copy from s2
- * Return: a pointer to the concatenated string, or NULL if malloc fails
+ *string_nconcat - concanates two strings
+ *@s1: the first string
+ *@s2: the second string
+ *@n: the number of bytes to take from s2
+ *Return: a pointer to the concanated strings else NULL
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, i = 0, k = 0;
-	char *result;
+	unsigned int i = 0, j = 0, m = 0, count;
+	char *str;
 
-	if (s1 == NULL)
+	if (!s1)
 		s1 = "";
-	if (s2 == NULL)
+	if (!s2)
 		s2 = "";
-
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-	if (n > len2)
-		n = len2;
-
-	result = malloc(sizeof(char) * (len1 + n + 1));
-	if (result == NULL)
+	while (*(s1 + i) != '\0')
+		i++;
+	while (*(s2 + j) != '\0')
+		j++;
+	if (n > j)
+		n = j;
+	count = i + n;
+	str = malloc(count + 1);
+	if (!str)
 		return (NULL);
-
-	while (s1[i])
+	for (m = 0; m < count; m++)
 	{
-		result[k] = s1[i];
-		i++;
-		k++;
+		if (m < i)
+			*(str + m) = *(s1 + m);
+		else
+			*(str + m) = *(s2 + m - i);
 	}
-
-	i = 0;
-	while (i < n)
-	{
-		result[k] = s2[i];
-		i++;
-		k++;
-	}
-
-	result[k] = '\0';
-	return (result);
+	*(str + m) = '\0';
+	return (str);
 }
