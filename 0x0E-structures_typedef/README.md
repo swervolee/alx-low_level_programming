aece6b99b1b3fc91fbafca317b15f67ac3568dff
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-17 07:28:52 +0000

    create a dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+This is a project about structure functions in c programming
+
+| task | function |
+| ---- | -------- |
+| task-0 | creating a dog.h file |
+| 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H
+
+/**
+ *struct dog - a dog structure
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner: the owner of the dog
+ *@description: a structure for a dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char*owner;
+};
+
+
+#endif/*_DOG_H*/
