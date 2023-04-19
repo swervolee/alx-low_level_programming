ecaf4fa51179404e4cc8d0aac53f4cc35aa86807
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-19 15:34:21 +0000

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n"), exit(1);
 	n_bytes = atoi(argv[1]);
 	if (n_bytes < 0)
-		printf("Error"), exit(2);
+		printf("Error\n"), exit(2);
 	for (i = 0; i < n_bytes; i++)
 	{
 		printf("%02x", p_code[i] & 0xFF);
