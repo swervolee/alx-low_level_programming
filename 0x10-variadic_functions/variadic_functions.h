3a12ceea3276ffd5c3c65f751bcb10625a6f6e66
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-20 20:13:21 +0000

    printing strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef VARIADIC_FUCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif/* VARIADIC_FUNCTIONS_H*/
