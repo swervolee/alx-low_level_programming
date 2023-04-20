852c489e067d32a4b8e0c068c1354ed4315195bd
Mona_Lisa <125455854+swervolee@users.noreply.github.com>
2023-04-20 18:11:14 +0000

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,7 +32,7 @@ void print_all(const char * const format, ...)
 		case 's':
 			s = va_arg(agstna, char *);
 			if (!s)
-				printf("(nil)");
+				printf("(nil)"), break;
 			printf("%s", s), j = 1;
 			break;
 
