b531f440d545b7f54ff7c4858a1434226aa01064
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-05-11 13:02:01 +0000

    upload keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Return: 0;
+ */
+
+int main(void)
+{
+	int limit = 2772, j, num;
+
+	srand(time(NULL));
+	for (i = 0; limit > 122; i++)
+	{
+		num = rand() % 127;
+		limit -= num;
+		printf("%c", num);
+	}
+	printf("%c", limit);
+}
