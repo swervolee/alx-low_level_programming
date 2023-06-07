ce78961e01c35c2b2c27cf9f31b625e99ab7522f
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-07 15:11:10 +0300

    will print the square root of 16777216

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,30 @@
-#include "main.h"
-
 /**
- *refine - a function to filter the square root
- *@n: the imput number
- *@half: the imput number divided by 2
- *Return: the square root if found else -1
+ * sqrt_recursive - A helper function to find the square root recursively
+ * @n: The input number
+ * @guess: The current guess for the square root
+ * @prev: The previous guess for the square root
+ * Return: The square root if found, -1 otherwise
  */
-
-int refine(int n, int half)
+int sqrt_recursive(int n, int guess, int prev)
 {
-	if (half * half == n)
-		return (half);
-	if (half * half < n)
-		return (-1);
-	return (refine(n, half - 1));
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess > n / guess)
+		return (prev);
+
+	return (sqrt_recursive(n, guess + 1, guess));
 }
 
 /**
- *_sqrt_recursion - a function to find the square root of number
- *@n: the input number
- *Return: -1 if the number has no square root else the square root
+ * _sqrt_recursion - A function to find the square root of a number
+ * @n: The input number
+ * Return: The square root if found, -1 otherwise
  */
-
 int _sqrt_recursion(int n)
 {
-	int (*func)(int a, int b);
-	int half;
+	if (n < 0)
+		return (-1);
 
-	half = n / 2;
-	func = refine;
-	if (half * half == n)
-		return (half);
-	else if (n == 1)
-		return (1);
-	else
-		return (func(n, half));
+	return (sqrt_recursive(n, 1, 0));
 }
