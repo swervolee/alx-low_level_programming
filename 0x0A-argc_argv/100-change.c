cfea70ce93a84a1fa9a9ee27e04b9180731dc62b
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-09 23:50:34 +0300

    counts the minimun amount of coins needed to give a change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *main - entry point
+ *  *@ac: arguement count
+ *   *@av: the arguement vector
+ *    *Return: zero
+ */
+
+int main(int ac, char **av)
+{
+	int array[] = {25, 10, 5, 2, 1}, i = 0, j = 0, count = 0;
+	int num = atoi(av[1]);
+
+	if (ac != 2 || ac == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= array[i])
+			count++, num -= array[i];
+		i++;
+	}
+	printf("%d\n", count);
+	return (0);
+}
