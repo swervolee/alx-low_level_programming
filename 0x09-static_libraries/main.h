ffa55a133b152fcc3b7b581062bbdfa55cc61dd3
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-10 01:20:15 +0300

    adds more files on the static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,6 +2,28 @@
 #define MAIN_H
 #include <stdio.h>
 
+
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+void print_number(int n);
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
 int main(void);
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
