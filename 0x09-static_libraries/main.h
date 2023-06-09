f0fa87fdaa4626da04efe64bc6c1a2cc0119a4ba
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-10 01:09:01 +0300

    static libraries

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int main(void);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
