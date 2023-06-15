178d9b75fbb761204e011987d4908775a68379dc
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-15 17:58:19 +0300

    allocate memory for integer array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,23 +9,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *allocated_memory;
-    unsigned int i;
-    unsigned int total_size;
+	void *allocated_memory;
+	unsigned int i;
+	unsigned int total_size;
+	char *ptr;
 
-    if (nmemb == 0 || size == 0)
-        return NULL;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	total_size = nmemb * size;
 
-    total_size = nmemb * size;
-
-    allocated_memory = malloc(total_size);
-
-    if (allocated_memory == NULL)
-        return NULL;
-
-    char *ptr = allocated_memory;
-    for (i = 0; i < total_size; i++)
-        ptr[i] = 0;
-
-    return allocated_memory;
+	allocated_memory = malloc(total_size);
+	if (allocated_memory == NULL)
+		return (NULL);
+	ptr = allocated_memory;
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
+	return (allocated_memory);
 }
