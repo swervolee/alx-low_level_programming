75e63e0b26c1582d47148add0c26764dc4168917
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-15 21:29:06 +0300

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int _putchar(char c);
 int _strlen(char *str);
 void _print_number(int *number, int size);
 void multiply(char *num1, char *num2);
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
