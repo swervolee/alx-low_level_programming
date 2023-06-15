e96295f5b169062fb4eed7bc87b3d5be87e64985
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-15 17:50:09 +0300

    concantinating strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s2 == NULL)
 		s2 = "";
-
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
+	for (s1_len = 0; s1[s1_len]; s1_len++)
+		;
+	for (s2_len = 0; s2[s2_len]; s2_len++)
+		;
 
 	if (n >= s2_len)
 		n = s2_len;
