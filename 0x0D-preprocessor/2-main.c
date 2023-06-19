8c97365e5cc03b67371fb3988c46baa4fde44ade
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-20 01:02:31 +0300

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: A program that prints the name of the file
+ * it was compiled from.
+ *
+ * Return: 0 always for success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
