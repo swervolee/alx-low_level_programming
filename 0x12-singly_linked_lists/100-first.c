d5fa66bde44400b78ac4b063574f4d052d94e9aa
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-30 22:08:44 +0300

    a fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include "lists.h"
-#include <stdio.h>
+/**
+ *before_main - prints a messsage before execution of the main function
+ */
+
 void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
