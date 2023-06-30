c82a2661c9e7b0776a9d684da646f51d5ad77eda
Mona <125455854+swervolee@users.noreply.github.com>
2023-06-30 13:41:37 +0300

    prints the elements of struct list_t

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *print_list - prints the contents of a linked list
+ *@h: pointer to the head of a linked
+ *Return: the number of elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = NULL;
+	size_t i = 0;
+
+
+	current = h;
+	while (current != NULL)
+	{
+		if (!current->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s", current->len, current->str);
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
