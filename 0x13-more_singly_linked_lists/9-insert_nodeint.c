3b498ef5539132d331c1691b93acf3d5793ed525
Mona <125455854+swervolee@users.noreply.github.com>
2023-07-04 23:14:01 +0300

    correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	current = *head;
 
-	while (current && count < idx - 1)
+	while (current && count < idx)
 	{
 		current = current->next;
 		count++;
