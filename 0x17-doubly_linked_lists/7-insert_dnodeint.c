1f49ca2c63f299cc37e97bd3108129c0a1abd9de
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-17 22:09:29 +0300

    a fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,42 +10,40 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **H, unsigned int idx, int n)
 {
-    dlistint_t *h, *new = NULL;
-    unsigned int i = 1;
+	dlistint_t *h, *new = NULL;
+	unsigned int i = 1;
 
-    h = *H;
-    if (idx == 0)
-    {
-        new = add_dnodeint(H, n);
-    }
-    else
-    {
-        if (h)
-        {
-            while (h->prev != NULL)
-                h = h->prev;
-            while (h != NULL && i < idx)
-            {
-                h = h->next;
-		i++;
-            }
-                if (h->next == NULL)
-                    new = add_dnodeint_end(H, n);
-                else
-                {
-			new = malloc(sizeof(dlistint_t));
-			if (!new)
-				return (NULL);
-			if (new)
+	h = *H;
+	if (idx == 0)
+	{
+		new = add_dnodeint(H, n);
+	}
+	else
+	{
+		if (h)
+		{
+			while (h->prev != NULL)
+				h = h->prev;
+			while (h != NULL && i < idx)
 			{
-				new->n = n;
-				new->next = h->next;
-				new->prev = h;
-				h->next->prev = new;
-				h->next = new;
+				h = h->next;
+				i++;
+			}
+			if (h->next == NULL)
+				new = add_dnodeint_end(H, n);
+			else
+			{
+				new = malloc(sizeof(dlistint_t));
+				if (new)
+				{
+					new->n = n;
+					new->next = h->next;
+					new->prev = h;
+					h->next->prev = new;
+					h->next = new;
+				}
 			}
 		}
-        }
-    }
-    return (new);
+	}
+	return (new);
 }
