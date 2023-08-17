06f0b56cc94527d1dff872980e94f5903f789fbc
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-17 23:34:02 +0300

    final code

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,44 +9,43 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *h, *trace;
+	dlistint_t *h1;
+	dlistint_t *h2;
 	unsigned int i;
 
-	h = *head;
-	trace = NULL;
+	h1 = *head;
 
-	if (h)
-	{
-		while (h->prev)
-			h = h->prev;
-	}
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-	if (index == 0)
-	{
-		if (h != NULL)
-			*head = h->next;
-		if (*head)
-			(*head)->prev = NULL;
-		free(h);
-		return (1);
-	}
+	i = 0;
 
-	i = 1;
-
-	while (h)
+	while (h1 != NULL)
 	{
 		if (i == index)
 		{
-			trace->next = h->next;
-			if (h->next)
-				h->next->prev = trace;
-			free(h);
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
 			return (1);
 		}
-
-		trace = h;
-		h = h->next;
+		h2 = h1;
+		h1 = h1->next;
 		i++;
 	}
+
 	return (-1);
 }
