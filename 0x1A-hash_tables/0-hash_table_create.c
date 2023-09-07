eed83ed2af89caacff39ef87b0d20a525ad359a0
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-08 01:01:07 +0300

    a fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new;
+
+
+	new = malloc(sizeof(hash_table_t));
 
 
 	if (new == NULL)
