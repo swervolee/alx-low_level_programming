8635380e4ea315acf9ffbe09363d51c6b66e4775
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-08 02:58:49 +0300

    a small fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,5 +11,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (!key || size == 0)
 		return (0);
-	return (hash_djb2(key));
+	return (hash_djb2(key) % size);
 }
