be9e8684a09c5da4f855d331dbe28cc29dac5b45
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-08 02:35:11 +0300

    generating a key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ *key_index - generates a key index for the hash table
+ *@key: the key
+ *@size: the size of the hash table
+ *Return: the key index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key || size == 0)
+		return (0);
+	return (hash_djb2(key));
+}
