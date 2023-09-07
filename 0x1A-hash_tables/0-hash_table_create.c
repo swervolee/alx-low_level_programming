8858c0f2b4e7fca6d853c93e962f100a39c5d0c6
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-07 23:43:59 +0300

    betty correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ *hash_table_create - creates a hash table
+ *@size: the size of the hash table
+ *Return: a hash table on success else NULL
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = malloc(sizeof(hash_table_t));
