6ae706b8e1a6ba6e2fb711e10797547a1b043157
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-08 00:50:49 +0300

    checker fail correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,6 +6,8 @@
  *Return: a hash table on success else NULL
  */
 
+
+/*
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
@@ -35,4 +37,30 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 
 	return (new);
+}
+*/
+
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	table->array = array;
+	table->size = size;
+
+	return (table);
 }
