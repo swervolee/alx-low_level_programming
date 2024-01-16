e8519680287f8c023e264b459634fb73a4aabb5e
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-16 11:20:44 +0300

    correction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; array && i < (int)size; i++)
 	{
-		printf("Value checked arrary[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
 			return (i);
