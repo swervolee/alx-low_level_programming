a93c297bd07e3a72eb7adfa1383caa41af25d95b
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-18 01:37:31 +0300

    final push

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,5 +1,5 @@
 #include "search_algos.h"
-
+#include <stdbool.h>
 /**
  *Recursive_binary - recursive binary search
  *@array: search array
@@ -14,34 +14,35 @@ int Recursive_binary(int *array, int left, int right, int value, int dflt)
 {
 	int mid, i;
 
-	i = left;
-
-	mid = (left + right) / 2;
+	i = left, mid = (left + right) / 2;
 
 	if (left > right || right >= dflt)
 		return (-1);
-
 	printf("Searching in array: ");
-
-	while (i < right)
+	while (true)
 	{
+		if (array[mid] == value)
+		{
+			if (array[mid - 1] == value)
+				printf("%d, ", array[mid]), mid--;
+			else if (array[mid - 1] != value && array[mid + 1] == value)
+			{
+				printf("%d\n", array[mid]);
+				return (mid);
+			}
+			else
+				return (mid);
+		}
+		if (i >= right)
+			break;
 		printf("%d, ", array[i]);
 		i++;
 	}
 	printf("%d\n", array[i]);
-
-	if (array[mid] == value)
-	{
-		if (array[mid - 1] == value)
-			return (Recursive_binary(array, mid - 1, right - 1,
-						 value, dflt));
-		return (mid);
-	}
 	if (array[mid] > value)
 		return (Recursive_binary(array, left, mid - 1, value, dflt));
 	if (array[mid]   < value)
 		return (Recursive_binary(array, mid + 1, right, value, dflt));
-
 	return (-1);
 }
 
